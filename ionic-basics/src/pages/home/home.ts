42de35ba41a3700b1be1c2bb7fcd2b65b2e33333
Munif <munifgebara@gmail.com>
2017-09-21 19:02:49 -0300

    ciclo de vida

--- ionic-basics/src/pages/home/home.ts
+++ ionic-basics/src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SobrePage } from '../sobre/sobre';
+import { DetalhePage} from '../sobre/detalhes/detalhe';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  dtl=DetalhePage;
 
   constructor(public navCtrl: NavController) {
 
--- ionic-basics/src/pages/sobre/sobre.ts
+++ ionic-basics/src/pages/sobre/sobre.ts
@@ -20,8 +20,57 @@ export class SobrePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  vaiDetalhe(s:string){
-    this.navCtrl.push(DetalhePage,{info:s}); //Qualquer coisa,objeto, apenas a string...
+  vaiDetalhe(s: string) {
+    this.navCtrl.push(DetalhePage, { info: s }); //Qualquer coisa,objeto, apenas a string...
   }
+
+
+  /*
+  
+    ionViewCanEnter(): boolean | Promise<void> {
+      console.log('ionViewCanEnter');
+      const rnd = Math.random();
+      return rnd > 0.1;
+    }
+  
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad');
+    }
+  
+    ionViewWillEnter() {
+      console.log('ionViewWillEnter');
+    }
+  
+    ionViewDidEnter() {
+      console.log('ionViewDidEnter');
+    }
+  
+    ionViewCanLeave(): boolean | Promise<void> {
+      console.log('ionViewCanLeave');
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, 1000);
+      });
+      return promise;
+    }
+  
+    ionViewWillLeave() {
+      console.log('ionViewWillLeave');
+    }
+  
+    ionViewDidLeave() {
+      console.log('ionViewDidLeave');
+    }
+  
+    ionViewWillUnload() {
+      console.log('ionViewWillUnload');
+    }
+  
+  
+  */
+
+
+
 
 }
